8500ecfff2fb63da52976eeda7ca04f817564510
Esteban Sanchez <estebansanr131@gmail.com>
2026-09-16 10:54:55 -0500

    feat: add database and note model setup

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
-const app = require ('./app');
+const app = require('./app');
+const connectDB = require('./db/connection');
 
-async function main (){
-    await app.listen(3000);
-    console.log('Server is running');
+async function main() {
+    await connectDB();
+
+    const PORT = process.env.PORT || 3000;
+    await app.listen(PORT);
+    console.log(`Server is running on port ${PORT}`);
 }
 
 main();
